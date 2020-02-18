b61d9039eb6933007a18fc72a093e636d76011b3
swmpkim <kimberly.cressman@dmr.ms.gov>
2020-02-18 11:54:16 -0600

    add data paths to notes01

--- notes_01.R
+++ notes_01.R
@@ -1,3 +1,10 @@
 # Notes/live coding from Day 1  
 
 # to be filled in as we go  
+
+
+
+# for 02_wrangling section:
+wq <- read.csv(here::here("data", "daily_wq.csv"), stringsAsFactors = FALSE)
+ebird <- read.csv(here::here("data", "eBird_workshop.csv"), stringsAsFactors = FALSE)
+ebird <- dplyr::distinct(ebird)

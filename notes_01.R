6dc948a806c26d07e10fa169a474e7cd0f27107f
swmpkim <kimberly.cressman@dmr.ms.gov>
2020-02-19 11:46:54 -0600

    filter

--- notes_01.R
+++ notes_01.R
@@ -1,10 +1,43 @@
 # Notes/live coding from Day 1  
 
 # to be filled in as we go  
-
+library(tidyverse)
 
 
 # 02_wrangling section ----
 wq <- read.csv(here::here("data", "daily_wq.csv"), stringsAsFactors = FALSE)
+
 ebird <- read.csv(here::here("data", "eBird_workshop.csv"), stringsAsFactors = FALSE)
+
 ebird <- dplyr::distinct(ebird)
+
+
+
+ebird2008 <- ebird %>%
+    filter(year == 2008)
+
+ebirdAK <- ebird %>%
+    filter(state == "AK")
+
+ebirdAK2008 <- ebird %>%
+    filter(state == "AK",
+           year == 2008)
+
+ebirdMS2018 <- ebird %>%
+    filter(year == 2018,
+           state == "MS")
+
+ebirdMSandAK <- ebird %>%
+    filter(state %in% c("AK", "MS"))
+
+
+my_states <- c("AK", "MS")
+ebird_again <- ebird %>%
+    filter(state %in% my_states)
+
+
+
+    
+    
+    
+    

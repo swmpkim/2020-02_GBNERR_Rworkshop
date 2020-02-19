1e77a22b15f44b382baec5f1b1e77233f0128823
swmpkim <kimberly.cressman@dmr.ms.gov>
2020-02-19 13:45:52 -0600

    is.na stuff

--- notes_01.R
+++ notes_01.R
@@ -1,7 +1,7 @@
 # Notes/live coding from Day 1  
 
 # to be filled in as we go  
-library(tidyverse)
+
 
 library(tidyverse)
 # 02_wrangling section ----
@@ -87,4 +87,26 @@ ebird %>%
     select(state, year, presence) %>% 
     filter(state %in% c("FL", "AL", "MS"),
            species == "American Coot")
+
+
+
+# your turn 3 ----
+
+wq_trimmed <- wq %>% 
+    select(station_code, month, day, temp, sal, do_pct, depth) %>% 
+    filter(!is.na(depth))
+
+
+summary(wq$depth)
+mean(wq$depth)
+mean(wq$depth, na.rm = TRUE)
+
+test <- c(1, 2, 3, 4, 5)
+test2 <- c(1, 2, NA, 4, 5)
+
+is.na(test)
+is.na(test2)
+is.na(wq$depth)
+
+!is.na(wq$depth)
 
